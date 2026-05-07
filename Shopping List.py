e1a2fae473f64626fadb2c07dca98a7c6c8c08d3
J-Mi <laloux_jm@hotmail.com>
2026-05-07 16:31:46 +0200

    save option for grocery list

--- Shopping List.py
+++ Shopping List.py
@@ -19,6 +19,12 @@ def load_grocery():
     return groceries
 groceries = load_grocery()
 
+def save_groceries(groceries):
+    with open(grocery_file,'w') as file:
+        for item in groceries:
+            file.write(item+"\n")
+    print(my_func.green("Grocery list saved!"))
+
 def show_list(g):
     print(my_func.blue("===Grocery List==="))
 
@@ -35,7 +41,7 @@ def add_item(g):
         print(my_func.red(f'You already have {new_item} in your list!'))
     else:
         g.append(new_item.lower())
-        print(my_func.blue(f"{new_item} is added to the groceries!"))
+        print(my_func.blue(f"{new_item} is added to your list!"))
 
 def remove_item(g):
     print(my_func.blue("===Remove item==="))
@@ -62,7 +68,7 @@ def clear_list(g):
 while True:
     print(my_func.green("===SHOPPING LIST MANAGER==="))
     try:
-        menu_choice = int(input("1. Add Item - 2. Remove Item - 3. View List - 4. Check/Uncheck Item - 5. Clear List - 6. Exit: "))
+        menu_choice = int(input("1. Add Item - 2. Remove Item - 3. View List - 4. Check/Uncheck Item - 5. Clear List - 6. Save My List - 7. Exit: "))
     except ValueError:
         print(my_func.red('Please enter a correct number'))
         continue
@@ -78,5 +84,7 @@ while True:
     if menu_choice == 5:
         clear_list(groceries)
     if menu_choice == 6:
+        save_groceries(groceries)
+    if menu_choice == 7:
         print("Thank you for your time! Have a nice day!")
         break
